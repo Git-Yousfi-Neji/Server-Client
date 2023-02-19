5e6271a53b7f13c50646a812783a24d531d9de2d
Git-Yousfi-Neji <yousfineji1@yahoo.com>
2023-02-19 23:14:27 +0000

    Update the authentication feature

--- client.cpp
+++ client.cpp
@@ -1,66 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
-#define SERVER_IP "127.0.0.1" // Change this to the server's IP address
-#define SERVER_PORT 8889
-#define MESSAGE_SIZE 1024
+#define PORT 8889
 
 int main() {
-    // Create the client socket
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        fprintf(stderr, "Error creating client socket\n");
+    int client_socket = 0;
+    struct sockaddr_in server_address;
+
+    char buffer[1024] = {0};
+    char username[1024], password[1024];
+    printf("Enter username: ");
+    scanf("%s", username);
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    // Create socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0) {
+        printf("\n Socket creation error \n");
         return -1;
     }
 
-    // Set up the server address
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server_address.sin_port = htons(SERVER_PORT);
+    server_address.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+        printf("\n Invalid address/ Address not supported \n");
+        return -1;
+    }
 
     // Connect to the server
-    if (connect(client_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
-        fprintf(stderr, "Error connecting to server\n");
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        printf("\n Connection Failed \n");
         return -1;
     }
 
-    // Start the message loop
-    char message[MESSAGE_SIZE];
-    while (1) {
-        // Get input from the user
-        printf("Enter a message: ");
-        fgets(message, MESSAGE_SIZE, stdin);
-        message[strcspn(message, "\n")] = 0; // Remove newline character from input
+    // send authentication message to server
+    char auth_msg[1024] = "auth ";
+    strcat(auth_msg, username);
+    strcat(auth_msg, " ");
+    strcat(auth_msg, password);
+    send(client_socket, auth_msg, strlen(auth_msg), 0);
 
-        // Send the message to the server
-        if (send(client_socket, message, strlen(message), 0) == -1) {
-            fprintf(stderr, "Error sending message to server\n");
-            return -1;
+    // wait for response from server
+    int valread = read(client_socket, buffer, 1024);
+    if (strcmp(buffer, "auth_success") == 0) {
+        printf("Authentication successful\n");
+        // continue with chat
+        while (1) {
+            char message[1024];
+            printf("Enter message: ");
+            fgets(message, 1024, stdin);
+            send(client_socket, message, strlen(message), 0);
         }
-
-        // Receive a message from the server
-        char server_message[MESSAGE_SIZE];
-        int message_length = recv(client_socket, server_message, MESSAGE_SIZE, 0);
-        if (message_length == -1) {
-            fprintf(stderr, "Error receiving message from server\n");
-            return -1;
-        } else if (message_length == 0) {
-            printf("Server disconnected\n");
-            break;
-        } else {
-            server_message[message_length] = '\0';
-            printf("Server says: %s\n", server_message);
-        }
+    } else {
+        printf("Authentication failed\n");
+        return -1;
     }
 
-    // Close the socket
     close(client_socket);
-
     return 0;
 }
+
--- server.cpp
+++ server.cpp
@@ -1,95 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
-#define SERVER_IP "127.0.0.1" 
-#define SERVER_PORT 8889
-#define MESSAGE_SIZE 1024
-#define MAX_PENDING_CONNECTIONS 5
-
-void handle_client(int client_socket) {
-    char message[MESSAGE_SIZE];
-    int message_length;
-
-    // Start the message loop
-    while (1) {
-        // Receive a message from the client
-        message_length = recv(client_socket, message, MESSAGE_SIZE, 0);
-        if (message_length == -1) {
-            fprintf(stderr, "Error receiving message from client\n");
-            break;
-        } else if (message_length == 0) {
-            printf("Client disconnected\n");
-            break;
-        } else {
-            message[message_length] = '\0';
-            printf("Client says: %s\n", message);
-        }
-
-        // Send a response message to the client
-        char response_message[MESSAGE_SIZE];
-        snprintf(response_message, MESSAGE_SIZE, "Received message: %s", message);
-        if (send(client_socket, response_message, strlen(response_message), 0) == -1) {
-            fprintf(stderr, "Error sending message to client\n");
-            break;
-        }
-    }
-
-    // Close the client socket
-    close(client_socket);
-}
+#define PORT 8889
+using namespace std;
 
 int main() {
-    // Create the server socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        fprintf(stderr, "Error creating server socket\n");
+    int server_socket = 0, client_socket = 0;
+    struct sockaddr_in server_address, client_address;
+    int addrlen = sizeof(server_address);
+
+    char buffer[1024] = {0};
+    char *auth_msg_prefix = "auth ";
+
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == 0) {
+        printf("Socket creation error\n");
         return -1;
     }
 
-    // Set up the server address
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
+    // Set server address to bind to
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(SERVER_PORT);
+    server_address.sin_port = htons(PORT);
 
-    // Bind the socket to the server address
-    if (bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address)) == -1) {
-        fprintf(stderr, "Error binding server socket\n");
+    // Bind the socket to the address and port
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        printf("Bind failed\n");
         return -1;
     }
 
     // Listen for incoming connections
-    if (listen(server_socket, MAX_PENDING_CONNECTIONS) == -1) {
-        fprintf(stderr, "Error listening for incoming connections\n");
+    if (listen(server_socket, 3) < 0) {
+        printf("Listen failed\n");
         return -1;
     }
 
-    printf("Server listening on port %d...\n", SERVER_PORT);
-
-    // Start accepting incoming connections
-    while (1) {
-        // Accept an incoming connection
-        struct sockaddr_in client_address;
-        socklen_t client_address_length = sizeof(client_address);
-        int client_socket = accept(server_socket, (struct sockaddr*) &client_address, &client_address_length);
-        if (client_socket == -1) {
-            fprintf(stderr, "Error accepting incoming connection\n");
-            break;
-        }
-
-        printf("Client connected: %s\n", inet_ntoa(client_address.sin_addr));
-
-        // Handle the client connection in a separate thread
-        handle_client(client_socket);
+    // Accept incoming connection
+    if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, (socklen_t*)&addrlen)) < 0) {
+        printf("Accept failed\n");
+        return -1;
     }
 
-    // Close the server socket
-    close(server_socket);
+    // wait for authentication message from client
+    int valread = read(client_socket, buffer, 1024);
+    if (strncmp(buffer, auth_msg_prefix, strlen(auth_msg_prefix)) == 0) {
+        // extract username and password from auth message
+        char *username = strtok(buffer + strlen(auth_msg_prefix), " ");
+        char *password = strtok(NULL, " ");
+        if (strcmp(username, "admin") == 0 && strcmp(password, "password") == 0) {
+            send(client_socket, "auth_success", strlen("auth_success"), 0);
+            printf("Authentication successful\n");
+        } else {
+            send(client_socket, "auth_failed", strlen("auth_failed"), 0);
+            printf("Authentication failed\n");
+            return -1;
+        }
+    } else {
+        printf("Invalid authentication message\n");
+        return -1;
+    }
 
+    // continue with chat
+    while (1) {
+        valread = read(client_socket, buffer, 1024);
+        printf("%s\n", buffer);
+    }
+
+    close(server_socket);
     return 0;
 }
